a7928c9d5553007bd4d3ec1c837e7539edb0319d
aaronsiim <web3aaron@gmail.com>
2023-06-15 14:18:16 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Aaron Sim on 2023-06-15.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
